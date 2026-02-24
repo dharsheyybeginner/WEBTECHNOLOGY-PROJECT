e22df41044579843d32c614fd30a7ac6c8b81541
Dharshini V <dharshini.v.2024.aiml@rajalakshmi.edu.in>
2026-02-24 08:38:34 +0530

    Add files via upload

---
+++ exp3.java
@@ -0,0 +1,28 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Set response content type
+        response.setContentType("text/html");
+
+        // Get the response writer
+        PrintWriter out = response.getWriter();
+
+        // Print Hello World
+        out.println("<html><body>");
+        out.println("<h1>Hello! World</h1>");
+        out.println("</body></html>");
+    }
+}
